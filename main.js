499c689d0733d81e2e771650ee1c0be395a7dbbe
CaiqueLudemann <caiqueludemann@gmail.com>
2023-08-11 11:17:08 -0300

    refactor code

--- main.js
+++ main.js
@@ -1,91 +1,85 @@
 const box = document.querySelector('.box');
 
-// || box property variables
-let diameter = 100;
-let border = 0;
-let marginLeft = 0;
-let marginRight = 100;
-let marginTop = 0;
+// || box dimensions & positions
+const boxDimensions = {
+  diameter: 100,
+  'border radius': 0,
+  'margin left': 0,
+  'margin top': 0,
+}
 
-//box dimensions
-box.style.width = `${diameter}px`;
-box.style.height = `${diameter}px`;
-box.style.borderRadius = `${border}px`;
+//assigning box dimensions
+box.style.width = `${boxDimensions.diameter}px`;
+box.style.height = `${boxDimensions.diameter}px`;
+box.style.borderRadius = `${boxDimensions['border radius']}px`;
 
-//box positions
-box.style.marginLeft = `${marginLeft}%`;
-box.style.marginTop = `${marginTop}%`;
-box.style.marginBottom = `${marginTop}%`;
+//assigning box positions
+box.style.marginLeft = `${boxDimensions['margin left']}%`;
+box.style.marginTop = `${boxDimensions['margin top']}%`;
+
 
 
 // || box-dimension functions
 
 //letterKeys
-const a = 65;
-const d = 68;
+const key = { a: 65, d: 68 };
 
-function aumentar(event){
-  if (event.keyCode === a) {
-    diameter += 10;
-    border += 10;
-    box.style.width = `${diameter}px`;
-    box.style.height = `${diameter}px`;
-    box.style.borderRadius = `${border}px`
-  }
+function increaseBoxSize() {
+  boxDimensions.diameter += 10;
+  boxDimensions['border radius'] += 10;
 };
 
-function diminuir(event){
-  if (event.keyCode === d) {
-    diameter -= 10;
-    border -= 10;
-    box.style.width = `${diameter}px`;
-    box.style.height = `${diameter}px`;
-    box.style.borderRadius = `${border}px`
-  }
+function decreaseBoxSize() {
+  boxDimensions.diameter -= 10;
+  boxDimensions['border radius'] -= 10;
 };
+
+function changeSize(event) {
+  switch (event.keyCode) {
+    case key.a:
+      increaseBoxSize();
+      box.style.width = `${boxDimensions.diameter}px`;
+      box.style.height = `${boxDimensions.diameter}px`;
+      box.style.borderRadius = `${boxDimensions['border radius']}px`
+      break;
+    case key.d:
+      decreaseBoxSize();
+      box.style.width = `${boxDimensions.diameter}px`;
+      box.style.height = `${boxDimensions.diameter}px`;
+      box.style.borderRadius = `${boxDimensions['border radius']}px`;
+      break;
+  }
+}
 
 // || box-position functions
 
 //arrowKeys
-const right = 39;
-const left = 37;
-const down = 40;
-const up = 38;
+const direction = { right: 39, left: 37, down: 40, up: 38 };
 
-function slideRight(event){
-  if (event.keyCode === right) {
-    marginLeft += 1;
-    box.style.marginLeft = `${marginLeft}%`
-  }
-};
-
-function slideLeft(event) {
-  if (event.keyCode === left) {
-    marginLeft -= 1;
-    box.style.marginLeft = `${marginLeft}%`
-  }
-};
-
-function slideBottom(event) {
-  if (event.keyCode === down) {
-    marginTop += 1;
-    box.style.marginTop = `${marginTop}%`
-  }
-};
-
-function slideTop(event) {
-  if (event.keyCode === up) {
-    marginTop -=1;
-    box.style.marginTop = `${marginTop}%`
+//checks keyCode pressed and moves box
+function slide(event) {
+  switch (event.keyCode) {
+    case direction.right:
+      boxDimensions['margin left'] += 1;
+      box.style.marginLeft = `${boxDimensions['margin left']}%`;
+      break;
+    case direction.left:
+      boxDimensions['margin left'] -= 1;
+      box.style.marginLeft = `${boxDimensions['margin left']}%`;
+      break;
+    case direction.down:
+      boxDimensions['margin top'] += 1;
+      box.style.marginTop = `${boxDimensions['margin top']}%`;
+      break;
+    case direction.up:
+      boxDimensions['margin top'] -= 1;
+      box.style.marginTop = `${boxDimensions['margin top']}%`;
+      break;
   }
 }
 
-//box-dimension event listeners
-window.addEventListener('keydown', aumentar)
-window.addEventListener('keydown', diminuir)
+//box-dimension event listener
+window.addEventListener('keydown', changeSize)
 
-//box-position event listeners
-window.addEventListener('keydown', slideRight)
-window.addEventListener('keydown', slideLeft)
-window.addEventListener('keydown', slideBottom)
-window.addEventListener('keydown', slideTop)
+//box-position event listener
+window.addEventListener('keydown', slide)
